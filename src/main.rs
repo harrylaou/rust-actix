a3c4ceba09a002846907957ffbb74cc7193d2611
Simplybook <harry@talos.software>
2020-03-31 11:25:44 +0300

    update versions

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,38 @@
-
-//! Example actix-web application.
-//!
-//! This code is adapted from the front page of the [Actix][] website.
-//!
 //! [actix]: https://actix.rs/docs/
 
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 use std::env;
 
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+// #[get("/")]
+#[get("/hello")]
+async fn greet() -> impl Responder {
+    // let to = req.match_info().get("name").unwrap_or("World");
+    HttpResponse::Ok().body("Hello world!")
 }
-
-fn main() {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     // Get the port number to listen on.
     let port = env::var("PORT")
         .unwrap_or_else(|_| "3000".to_string())
         .parse()
         .expect("PORT must be a number");
 
-    // Start a server, configuring the resources to serve.
-    server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
-    })
-    .bind(("0.0.0.0", port))
-    .expect("Can not bind to port 8000")
-    .run();
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| {
+        App::new().service(greet)
+        // .route("/", |r| r.f(greet))
+        // .route("/{name}", |r| r.f(greet))
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server
+            .bind(("0.0.0.0", port))
+            .expect("Can not bind to port 8000")
+    };
+
+    server.run().await
 }
